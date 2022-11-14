d3f84696d8cfd503d599ff5222a9ab02185fe877
anikait <anikaitth@gmail.com>
2022-11-15 01:48:41 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import os  # accessing the os functions
+
+import check_camera
+
+def title_bar():
+
+    print("\t**********************************************")
+    print("\t***** Face Recognition Attendance System *****")
+    print("\t**********************************************")
+
+
+# creating the user main menu function
+
+def mainMenu():
+    title_bar()
+    print()
+    print(10 * "*", "WELCOME MENU", 10 * "*")
+    print("[1] Check Camera")
+    print("[2] Capture Faces")
+    print("[3] Train Images")
+    print("[4] Recognize & Attendance")
+    print("[5] Auto Mail")
+    print("[6] Quit")
+
+    while True:
+        try:
+            choice = int(input("Enter Choice: "))
+
+            if choice == 1:
+                checkCamera()
+                break
+            elif choice == 6:
+                print("Thank You")
+                break
+            else:
+                print("Invalid Choice. Enter 1-4")
+                mainMenu()
+        except ValueError:
+            print("Invalid Choice. Enter 1-4\n Try Again")
+    exit
+
+
+# ---------------------------------------------------------
+# calling the camera test function from check camera.py file
+
+def checkCamera():
+    check_camera.camera()
+    key = input("Enter any key to return main menu")
+    mainMenu()
+
+
+# ---------------main driver ------------------
+mainMenu()
+
